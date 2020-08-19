809d8b4852f103108a3fb942c84c70b3172f37be
alvion4ik <alvion4ik@gmail.com>
2020-08-19 17:16:55 +0300

    5 homwork

--- project_mDB/js/script.js
+++ project_mDB/js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -24,3 +10,27 @@ const movieDB = {
     ]
 };
 
+
+const promoAdv = document.querySelectorAll('.promo__adv img'),
+    promoBg = document.querySelector('.promo__bg'),
+    promoGenre = promoBg.querySelector('.promo__genre'),
+    promoInteractiveList = document.querySelector('.promo__interactive-list');
+
+promoAdv.forEach( i => {
+    i.remove();
+});
+
+promoGenre.textContent = 'ДРАМА';
+
+promoBg.style.cssText = 'background: url("img/bg.jpg") center center/cover no-repeat; background-position: top;';
+
+promoInteractiveList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((key, i) => {
+    promoInteractiveList.innerHTML += 
+    `<li class="promo__interactive-item">${i + 1} ${key}
+        <div class="delete"></div>
+    </li>`;
+});
