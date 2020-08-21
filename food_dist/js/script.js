9e6ad2c6fb0892c1e1525388d482ebf03548e183
alvion4ik <alvion4ik@gmail.com>
2020-08-21 13:44:25 +0300

    7

---
+++ food_dist/js/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () =>{
+
+  const tabs = document.querySelectorAll('.tabheader__item'),
+    tabContent = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__items');
+  
+  const hideTabContent = (arrayHide, arraActiveRemove) => {
+    arrayHide.forEach(key => {
+      key.style.display = 'none';
+    });
+    
+    arraActiveRemove.forEach(key => {
+      key.classList.remove('tabheader__item_active');
+    });
+  };
+
+  const showTabContent = (arrayShow,arraActiveAdd,i = 0) => {
+    arrayShow[i].style.display = 'block';
+    arraActiveAdd[i].classList.add('tabheader__item_active');
+  };
+
+  hideTabContent(tabContent,tabs);
+  showTabContent(tabContent,tabs);
+
+  tabsParent.addEventListener('click', e => {
+    const target = e.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((key, i) => {
+        if (target == key) {
+          hideTabContent(tabContent,tabs);
+          showTabContent(tabContent,tabs,i);
+        }
+      });
+    }
+  });
+});
