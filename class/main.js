17c8a22af6c324541a3967ad1e200c2324509a2a
alvion4ik <alvion4ik@gmail.com>
2020-08-22 12:51:42 +0300

    class

--- class/main.js
+++ class/main.js
@@ -17,10 +17,20 @@ class ColoredRectangleWithText extends Rectangle {
     this.text = text;
     this.bgColor = bgColor;
   }
+
+  showMyProps(){
+    console.log(`Текст: ${this.text}, цвет ${this.bgColor}`);
+  }
 }
 
-const square = new Rectangle(10, 10);
-const long = new Rectangle(20,100);
+const div = new ColoredRectangleWithText(25, 10, ' hello world', 'red');
 
-console.log(square.calcArea());
-console.log(long.calcArea());
+div.showMyProps();
+console.log(div.calcArea());
+
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20,100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
